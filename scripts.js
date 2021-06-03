05b89842c588e5268846b8ab7372aa464c62eabc
MohammaZakaria <zakariamohammad02@gmail.com>
2021-06-03 20:04:28 +0300

    latest update and thats is

--- scripts.js
+++ scripts.js
@@ -9,25 +9,25 @@
 //         navbar.classList.remove("sticky");
 //     }
 // }
-setInterval(() => {
-    const images = document.querySelectorAll('.slide')
-    images.forEach((img, index) => {
-        const imagesCount = images.length - 1
-        let tracker = 0
-        console.log(img.classList.contains('fade-in-image'));
-        if (img.classList.contains('fade-in-image')) {
-            if (index < imagesCount) {
-                console.log(img, index + ' out of ' + imagesCount)
-                tracker = index + 1
-                img.classList.remove('fade-in-image')
-                images[tracker].classList.add('fade-in-image')
-            } else if (index >= imagesCount) {
-                img.classList.remove('fade-in-image')
-                images[0].classList.add('fade-in-image')
-            }
-        }
-    })
-}, 3000);
+// setInterval(() => {
+//     const images = document.querySelectorAll('.slide')
+//     images.forEach((img, index) => {
+//         const imagesCount = images.length - 1
+//         let tracker = 0
+//         console.log(img.classList.contains('fade-in-image'));
+//         if (img.classList.contains('fade-in-image')) {
+//             if (index < imagesCount) {
+//                 console.log(img, index + ' out of ' + imagesCount)
+//                 tracker = index + 1
+//                 img.classList.remove('fade-in-image')
+//                 images[tracker].classList.add('fade-in-image')
+//             } else if (index >= imagesCount) {
+//                 img.classList.remove('fade-in-image')
+//                 images[0].classList.add('fade-in-image')
+//             }
+//         }
+//     })
+// }, 3000);
 
 // const buttons = document.querySelectorAll('#filter-movieActor button')
 
